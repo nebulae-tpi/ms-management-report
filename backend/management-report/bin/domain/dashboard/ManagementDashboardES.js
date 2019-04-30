27e4e04f995a160ca3b0f4e4b661bddd2074820b
juansantane <juan.santa@nebulae.com.co>
2019-04-30 16:53:07 -0500

    force deployment

--- backend/management-report/bin/domain/dashboard/ManagementDashboardES.js
+++ backend/management-report/bin/domain/dashboard/ManagementDashboardES.js
@@ -7,7 +7,7 @@ const MATERIALIZED_VIEW_TOPIC = "emi-gateway-materialized-view-updates";
 const Event = require("@nebulae/event-store").Event;
 const eventSourcing = require("../../tools/EventSourcing")();
 const uuidv4 = require("uuid/v4");
-const WalletDA = require("./data-access/ManagementDashboardDA");
+const DashboardDA = require("./data-access/ManagementDashboardDA");
 
 /**
  * Singleton instance
@@ -19,10 +19,61 @@ class ManagementDashboardCQRS {
 
   }
 
-  handleVehicleSubscriptionPaid$({ aid, data, user }){
-    console.log({aid, data, user});
-    return of(aid);
+  handleVehicleSubscriptionPaid$({ et, etv, at, aid, user, timestamp, av, data }){
+    const { licensePlate, packProduct, quantity, amount, daysPaid } = data;
+    const { year, monthStr, month, week, dayOfWeek, dayOfWeekStr, dayOfYear, dayOfMonth, hourOfDay, minute, second } = this.decomposeTime(timestamp);
+
+    const fieldsToSet = [['lastUpdate', Date.now()]];
+    const fieldsToInc = [];
+    
+    fieldsToInc.push([`subscription.payment.count`, 1]);
+    fieldsToInc.push([`subscription.payment.days`, daysPaid]);
+    fieldsToInc.push([`subscription.payment.value`, amount ]);
+
+    return forkJoin(
+      // YEAR
+      DashboardDA.updateTimeBox$(
+        [ ['businessId', businessId], ['timespanType', 'YEAR'], ['YEAR', year] ], fieldsToSet, fieldsToInc
+      ),
+      // MONTH
+      DashboardDA.updateTimeBox$(
+        [ ['businessId', businessId], ['timespanType', 'MONTH'], ['YEAR', year], ['MONTH', month] ],
+        fieldsToSet, fieldsToInc, [['MONTH_NAME', monthStr]]
+      ),
+      // WEEK
+      DashboardDA.updateTimeBox$(
+        [ ['businessId', businessId], ['timespanType', 'WEEK'], ['YEAR', year], ['MONTH', month], ['WEEK', week] ],
+        fieldsToSet, fieldsToInc
+      ),
+      // DAY
+      DashboardDA.updateTimeBox$(
+        [ ['businessId', businessId], ['timespanType', 'DAY'], ['YEAR', year], ['MONTH', month], ['DAY', dayOfMonth] ],
+        fieldsToSet, fieldsToInc,
+        [ ['WEEK', week], ['DAY_OF_YEAR', dayOfYear], ['DAY_NAME', dayOfWeekStr], ['DAY_OF_WEEK', dayOfWeek] ]
+      ),
+    );
   }
+
+  decomposeTime(ts) {
+    //2018-12-4 17:12:05
+    const date = new Date(new Date(ts).toLocaleString('es-CO', { timeZone: 'America/Bogota' }));
+    const { year, week, day } = gregorian.weekNumberYear(date);
+    const daysOfWeek = ['MON', 'TUE', 'WED', 'THU', 'FRY', 'SAT', 'SUN'];
+    const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC',];
+    return {
+        year,
+        monthStr: months[date.getMonth()],
+        month: date.getMonth() + 1,
+        week,
+        dayOfWeek: day,
+        dayOfWeekStr: daysOfWeek[day - 1],
+        dayOfYear: gregorian.dayOfYear(date),
+        dayOfMonth: date.getDate(),
+        hourOfDay: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds()
+    };
+}
 
 }
 
--- backend/management-report/bin/domain/dashboard/data-access/ManagementDashboardDA.js
+++ backend/management-report/bin/domain/dashboard/data-access/ManagementDashboardDA.js
@@ -3,7 +3,7 @@
 require("datejs");
 let mongoDB = undefined;
 //const mongoDB = require('./MongoDB')();
-const COLLECTION_NAME = "Wallet";
+const COLLECTION_NAME = "TimeBox";
 const { CustomError } = require("../../../tools/customError");
 const { map, mergeMap, reduce, tap } = require("rxjs/operators");
 const { of, Observable, defer, from, range } = require("rxjs");
@@ -21,6 +21,33 @@ class ManagementDashboardDA {
       observer.complete();
     });
   }
+
+  static updateTimeBox$(keys, fieldsToSet, fieldsToInc, secondaryKeys = []) {
+    const collection = mongoDB.db.collection(COLLECTION_NAME);
+    const _id = keys.map(([k, v]) => v).join('_');
+    const [query, update,opt] = [
+      { _id },
+      {
+        '$setOnInsert': { 
+          timestamp: Date.now(),
+          ...keys.reduce((obj, [key, value]) => { obj[key] = value; return obj; }, {}),
+          ...secondaryKeys.reduce((obj, [key, value]) => { obj[key] = value; return obj; }, {})
+        },
+      },
+      { 'multi': false, upsert: true }
+    ];
+    if( fieldsToInc.length > 0){      
+      update['$inc'] = fieldsToInc.reduce((obj, [key, value]) => { obj[key] = value; return obj; }, {});
+    }
+    if( fieldsToSet.length > 0){
+      update['$set'] = fieldsToSet.reduce((obj, [key, value]) => { obj[key] = value; return obj; }, {});
+    }
+
+    return defer(() => collection.update(query, update,opt)).pipe(
+      tap(x => { if (x.result.ok !== 1) throw (new Error(`timeBox(id:${id}) updated failed`)); }),
+    );
+  }
+
   
 }
 /**
