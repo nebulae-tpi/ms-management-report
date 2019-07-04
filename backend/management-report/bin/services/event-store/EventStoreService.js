1748ef4ab3d752311658a33751bfaf1fe4182df2
juansantane <juan.santa@nebulae.com.co>
2019-07-04 09:53:01 -0500

    listener for SaleWalletRechargeCommited event

--- backend/management-report/bin/services/event-store/EventStoreService.js
+++ backend/management-report/bin/services/event-store/EventStoreService.js
@@ -10,7 +10,7 @@ let instance;
 /**
  * Micro-BackEnd key
  */
-const mbeKey = "ms-management-report_mbe_management-report_006";
+const mbeKey = "ms-management-report_mbe_management-report_007";
 
 class EventStoreService {
   constructor() {
