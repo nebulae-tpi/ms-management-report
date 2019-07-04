b497cff4b051d26931b6834cb5cd5a18b46215d6
juansantane <juan.santa@nebulae.com.co>
2019-07-04 09:23:36 -0500

    listener for SaleWalletRechargeCommited event

--- backend/management-report/bin/server.js
+++ backend/management-report/bin/server.js
@@ -13,6 +13,7 @@ const graphQlService = require('./services/emi-gateway/GraphQlService')();
 const ManagementDashboard = require("./domain/dashboard");
 const { concat, forkJoin } = require('rxjs');
 
+
 const start = () => {
     concat(
         eventSourcing.eventStore.start$(),
--- backend/management-report/bin/services/event-store/EventStoreService.js
+++ backend/management-report/bin/services/event-store/EventStoreService.js
@@ -10,7 +10,7 @@ let instance;
 /**
  * Micro-BackEnd key
  */
-const mbeKey = "ms-management-report_mbe_management-report_004";
+const mbeKey = "ms-management-report_mbe_management-report_005";
 
 class EventStoreService {
   constructor() {
