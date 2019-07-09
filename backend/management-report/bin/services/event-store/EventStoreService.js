d612128d585c8b4c303a8c1c1871da6a9ff602f0
juansantane <juan.santa@nebulae.com.co>
2019-07-09 14:43:28 -0500

    timestamp fixed

--- backend/management-report/bin/services/event-store/EventStoreService.js
+++ backend/management-report/bin/services/event-store/EventStoreService.js
@@ -10,7 +10,7 @@ let instance;
 /**
  * Micro-BackEnd key
  */
-const mbeKey = "ms-management-report_mbe_management-report_011";
+const mbeKey = "ms-management-report_mbe_management-report_012";
 
 class EventStoreService {
   constructor() {
