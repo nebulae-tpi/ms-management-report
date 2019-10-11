e31f6b1543d9e336ca2b26de3af02c5246f42929
juansantane <juan.santa@nebulae.com.co>
2019-10-11 15:36:26 -0500

    timestamp fixed

--- backend/management-report/bin/domain/dashboard/ManagementDashboardCQRS.js
+++ backend/management-report/bin/domain/dashboard/ManagementDashboardCQRS.js
@@ -37,7 +37,9 @@ class ManagementDashboardCQRS {
       businessId = authToken.businessId;
     }
 
-    return ManagementDashboardDA.getBusinessSummaryReport$(businessId, timestampType, initDate, endDate)
+    const initTimestamp = new Date(new Date(initDate).toLocaleString('es-CO', { timeZone: 'America/Bogota' })).getTime();
+    const endTimestamp = new Date(new Date(endDate).toLocaleString('es-CO', { timeZone: 'America/Bogota' })).getTime();
+    return ManagementDashboardDA.getBusinessSummaryReport$(businessId, timestampType, initTimestamp, endTimestamp)
     .pipe(
       // tap(r => console.log("precarga", JSON.stringify(r))),
       mergeMap(result => from(result)
